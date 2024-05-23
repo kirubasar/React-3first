ace476b3ac402bd8671fa84a94eb411e5558afc6
kirubasar <kiruba.sri.official@gmail.com>
2024-05-23 12:20:10 +0530

    conditional rendering method 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,10 @@
   const App = () => {
-    const friends = [
-   {id: 1, name: 'John',age: 30},
-   {id: 2, name: 'peter', age: 20}
-    ]
-    const listFriends = [];
-    for (let i = 0; i < friends.length; i++) {
-      listFriends.push(
-        <li key={friends[i].id}>{friends[i].name} {friends[i].age}
-        </li>
-      )
-    }
-  return (
-    <div>
-     <h1>Friends</h1> 
-     <ul>
-      {
-        friends.map((friend, index) =>
-          <li key={index}>{friend.name} {friend.age}</li>
-        )
-      }
-     </ul>
-  </div>
-  )
+    const isLoggedIn = false; 
+ if(isLoggedIn) {
+  return <h1>Welcome back!</h1>;
+} else{
+  return <h1>Please log in.</h1>;
+}
 }
   
 export default App;
