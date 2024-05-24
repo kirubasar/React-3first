aa72017e61042eef281513f67b164073a2631e14
kirubasar <kiruba.sri.official@gmail.com>
2024-05-24 12:13:33 +0530

    multiple component

--- src/App.jsx
+++ src/App.jsx
@@ -20,16 +20,24 @@ return (
 
  export default App;*/
 import { Component } from "react";
+class Hello extends Component{
+  render(){
+    return(
+      <h1>Hello World</h1>
+    )
+  }
+}
  //Class Component
- class App extends Component{
+ class App  extends Component{
   // override the render method
   render(){
     //return JSX
     return (
       <div>
-    <h1> My React App</h1>
-  </div>
-    )
+        <Hello />
+      </div>
+    
+   )
   }
  }
  export default App;
