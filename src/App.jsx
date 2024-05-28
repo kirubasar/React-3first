b9621f70779c4a39e92f1aabc47465de89aee717
kirubasar <kiruba.sri.official@gmail.com>
2024-05-28 12:45:37 +0530

    react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,72 +1,25 @@
-/*class Hello extends Component {
-  componentDidMount() {
-    console.log('Hello component mounted');
-  }
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
-  componentWillUnmount(){
-    console.log('Hello component unmounted')
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <h1>Hello, World!</h1>
+  },
+  {
+    path: "/contact",
+    element: <h1>Contact Us</h1>
+  },
+  {
+    pathe: "/services",
+    element: <h1>Our Services</h1>
+  },
+  {
+    path: "/products",
+    element: <h1>Our Products</h1>
   }
-  render() {
-    return(
-      <h1>Hello World!</h1>
-    )
-  }
+]);
+
+const App = () => {
+  return <RouterProvider router={router}/>;
 }
-
-export class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      showHello:true
-    }
-  }
-
-  handleToggle = () => {
-    this.setState({
-      showHello: !this.state.showHello
-    })
-  }
-  render() {
-    return (
-      <div>
-         <button onClick ={this.handleToggle}>Toggle Hello</button>
-         {this.state.showHello && <Hello />}
-         </div>
-    )  
-  }
-  
-}
-export default App;*/
-
-
-import { useEffect, useState } from 'react'
-
-const App= ()=> {
-  let [count, setCount] =useState(0);
-useEffect(() =>{
-  console.log('Component mounted')
-})
-
-
-  const handleIncrement = () => {
-    setCount(count+1)
-  }
-  const handleDecrement = () => {
-    if(count!=0)
-    setCount(count-1)
-  }
-  const reset = () => {
-    setCount(0)
-  }
-
-  return (
-      <div>
-        <h1>Counter: {count}</h1>
-        <button onClick={handleIncrement}>Increment</button>
-        <button onClick={handleDecrement}>Decrement</button>
-        <button onClick={reset}>Reset</button>
-      </div>
-    )
-  } 
-
-export default App
+export default App;
