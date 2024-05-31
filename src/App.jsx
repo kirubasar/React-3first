5e3d2172572881f7db2c6d8dd2ddf17f0b76023e
kirubasar <kiruba.sri.official@gmail.com>
2024-05-31 11:22:34 +0530

    context state change value

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
+import { useEffect } from 'react';
 import { useContext } from 'react';
 import { useState } from 'react';
 import { createContext } from 'react'
 const NameContext = createContext();
 const D = ()=>{
-    const {name} = useContext(NameContext)
+    const {name, setName} = useContext(NameContext)
+    useEffect(()=>{
+        setTimeout(() =>{
+           setName('Kiruba');
+        }, 5000);
+    })
     return(
         <div>
             <h1>Hello, {name}!</h1>
@@ -26,7 +32,7 @@ const B =() =>{
 const App =() => {
     const [name, setName] = useState('sri')
   return (
-    <NameContext.Provider value={{name}}>
+    <NameContext.Provider value={{name, setName}}>
         <B/>
     </NameContext.Provider>
   )
