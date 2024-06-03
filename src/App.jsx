9ccfdd29602f5ad68ab261fe1c4031caec27ef76
kirubasar <kiruba.sri.official@gmail.com>
2024-06-03 11:45:20 +0530

    eg3

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,25 @@
-import { useRef, useState } from "react"
-
+import React, { useRef, useState } from 'react'
 
 const App = () => {
-  const [state, setState] = useState(0);
-  const inputRef = useRef(null);
+  const [count, setCount] = useState(0);
+  const countRef = useRef(0);
   const handleClick =()=>{
-    //console.log(inputRef.current.value)
-    inputRef.current.focus()
+    setCount(count+1)
   }
-  const handleChange = ()=>{
-    inputRef.current.value = 'Hello World';
-    setState(state+1);
+  const handleRefClick=()=>{
+    countRef.current +=1;
+    console.log(countRef.current)
   }
-  console.log('rendering component');
+  console.log('component rendered');
+
   return (
-    <>
-      <input
-      type='text'
-      ref={inputRef}
-      />
-      <button onClick={handleClick}>Focus Input</button>
-      <button onClick={handleChange}>Change Value</button>
-    </>
+    <div>
+      <h1>State Count: {count}</h1>
+      <button onClick ={handleClick}>Increment State Count</button>
+      <h1>Ref Count:{countRef.current}</h1>
+      <button onClick={handleRefClick}>Increment Ref Count</button>
+    </div>
   )
 }
 
-export default App;
+export default App
