7a6db673ea9839392bca16640bf50e18f5c2c0b6
kirubasar <kiruba.sri.official@gmail.com>
2024-06-03 11:26:18 +0530

    eg2

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
-import { useRef } from "react"
+import { useRef, useState } from "react"
 
 
 const App = () => {
+  const [state, setState] = useState(0);
   const inputRef = useRef(null);
   const handleClick =()=>{
     //console.log(inputRef.current.value)
     inputRef.current.focus()
   }
+  const handleChange = ()=>{
+    inputRef.current.value = 'Hello World';
+    setState(state+1);
+  }
+  console.log('rendering component');
   return (
     <>
       <input
@@ -14,6 +20,7 @@ const App = () => {
       ref={inputRef}
       />
       <button onClick={handleClick}>Focus Input</button>
+      <button onClick={handleChange}>Change Value</button>
     </>
   )
 }
